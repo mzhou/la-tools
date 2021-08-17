c5c725f203a25e0efaaaa1273f4ef5c1dbf6049c
Michael Zhou <mzhou@cse.unsw.edu.au>
2021-08-18 00:46:49 +0900

    more progress

--- fast-dl/src/lib.rs
+++ fast-dl/src/lib.rs
@@ -1,10 +1,13 @@
+use std::collections::BTreeSet;
 use std::error::Error;
 use std::ffi::OsString;
-use std::io::{Read, Write};
+use std::fmt::{Display, Formatter, Result as FmtResult};
 
 use clap::Clap;
 use ini::Ini;
 use reqwest::Client;
+
+use la_tools::git_index;
 
 #[derive(Clap)]
 struct Opts {
@@ -15,6 +18,20 @@ struct Opts {
     #[clap(long, default_value = "64")]
     network_threads: usize,
 }
+
+#[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
+enum MainError {
+    InvalidVersionIni,
+    InvalidGitIndex,
+}
+
+impl Display for MainError {
+    fn fmt(self: &Self, f: &mut Formatter) -> FmtResult {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for MainError {}
 
 #[tokio::main]
 pub async fn try_main<I, T>(itr: I) -> Result<i32, Box<dyn Error>>
@@ -37,13 +54,46 @@ where
         .await?;
 
     let version_ini = Ini::load_from_str(&version_ini_str)?;
-    let index_str = version_ini.get_from(Some("VERSION"), "INDEX").unwrap_or("");
+    let index_name_str = version_ini
+        .get_from(Some("VERSION"), "INDEX")
+        .ok_or(MainError::InvalidVersionIni)?;
 
-    eprintln!("Current version is {}", index_str);
+    eprintln!("Current version is {}", index_name_str);
 
-    if index_str.is_empty() {
-        eprintln!("Invalid  VERSION.INDEX in version.ini");
+    if index_name_str.is_empty() {
+        eprintln!("Invalid VERSION.INDEX in version.ini");
         return Ok(1);
+    }
+
+    eprintln!("Downloading index");
+    let index_bytes = client
+        .get(format!(
+            "http://la.cdn.gameon.jp/la/patch/{}",
+            &index_name_str
+        ))
+        .send()
+        .await?
+        .bytes()
+        .await?;
+    let index = git_index::parse(&index_bytes).ok_or(MainError::InvalidGitIndex)?;
+
+    eprintln!("Index defines {} files", index.entries.len());
+
+    eprintln!("Calculating directories");
+
+    let dirs: BTreeSet<String> = index
+        .entries
+        .iter()
+        .filter_map(|e| {
+            let s = std::str::from_utf8(e.name).ok()?;
+            Some(s[..s.rfind('/')?].into())
+        })
+        .collect();
+
+    eprintln!("Found {} directories:", dirs.len());
+
+    for d in dirs.iter() {
+        eprintln!("    {}", d);
     }
 
     Ok(0)
